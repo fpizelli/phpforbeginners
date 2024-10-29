d6490c4cef586bc5a0c50f6208f08fc640882371
fpizelli <168946758+fpizelli@users.noreply.github.com>
2024-10-29 16:29:50 -0300

    Create first PHP tag

---
+++ websites/demo/index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Demo</title>
+</head>
+<body>
+
+    <h1>
+        <?php
+            echo "Hello, Mr. Bluesky!";
+        ?>
+    </h1>
+
+    <p>
+        <?php
+
+            echo "This is a paragraph. Apparently.";
+
+        ?>
+    </p>
+
+</body>
+</html>
