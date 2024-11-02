973019c90a1d4eaa5dbe702748bd6e7f40d4a3ef
fpizelli <168946758+fpizelli@users.noreply.github.com>
2024-11-02 02:43:50 -0300

    Create page links

--- websites/demo/about.view.php
+++ websites/demo/about.view.php
@@ -95,8 +95,8 @@
                     </div>
                 </div>
                 <div class="text-center">
-                    <h1 class="text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">Data to
-                        enrich your online business</h1>
+                    <h1 class="text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">This is the
+                        About Page</h1>
                     <p class="mt-8 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">Anim aute id magna aliqua
                         ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam
                         occaecat.</p>
