b46664ce8f2ca116ec37108a75dc58c50577acbf
fpizelli <168946758+fpizelli@users.noreply.github.com>
2024-11-04 05:56:18 -0300

    Finish episode 18

---
+++ websites/demo/Database.php
@@ -0,0 +1,18 @@
+<?php
+
+class Database {
+    public $connection;
+
+    public function __construct() {
+        $dsn = "mysql:host=127.0.0.1;port=3305;dbname=myapp;user=root;charset=utf8mb4";
+
+        $this->connection = new PDO($dsn);
+    }
+
+    public function query($query) {
+        $statement = $this->connection->prepare($query);
+        $statement->execute();
+
+        return $statement;
+    }
+}
--- websites/demo/index.php
+++ websites/demo/index.php
@@ -1,18 +1,9 @@
 <?php
 
 require 'functions.php';
-// require 'router.php';
+require 'Database.php';
 
-// connect to our MySQL database.
-$dsn = "mysql:host=127.0.0.1;port=3305;dbname=myapp;user=root;charset=utf8mb4";
+$db = new Database();
+$posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
 
-$pdo = new PDO($dsn);
-
-$statement = $pdo->prepare("select * from posts");
-$statement->execute();
-
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-foreach ($posts as $post) {
-    echo "<li>" . $post['title'] . "</li>";
-}
+dd($posts['title']);
