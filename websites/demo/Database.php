4c4c0b3bb3d73b0fd13472182572baa5a22c9b5d
fpizelli <168946758+fpizelli@users.noreply.github.com>
2024-11-05 14:29:45 -0300

    Finish episode 20

--- websites/demo/Database.php
+++ websites/demo/Database.php
@@ -11,9 +11,9 @@ class Database {
         ]);
     }
 
-    public function query($query) {
+    public function query($query, $params = []) {
         $statement = $this->connection->prepare($query);
-        $statement->execute();
+        $statement->execute($params);
 
         return $statement;
     }
